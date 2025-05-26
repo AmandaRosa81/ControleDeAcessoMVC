26e5e8a517d9f0ab51e0c574941a0115e703b9c2
Gabrieli da Silva Marcelino <gabih.12161@gmail.com>
2025-05-26 08:37:59 -0300

    Criação da entidade Horário Base - 26/05/2025

--- src/main/java/com/senai/ProjetoControleDeAcesso/Controller/HorarioBaseController.java
+++ src/main/java/com/senai/ProjetoControleDeAcesso/Controller/HorarioBaseController.java
@@ -1,4 +1,6 @@
 package com.senai.ProjetoControleDeAcesso.Controller;
 
 public class HorarioBaseController {
+
+
 }
--- src/main/java/com/senai/ProjetoControleDeAcesso/Model/Horario/HorarioBase.java
+++ src/main/java/com/senai/ProjetoControleDeAcesso/Model/Horario/HorarioBase.java
@@ -1,4 +1,43 @@
 package com.senai.ProjetoControleDeAcesso.Model.Horario;
 
-public class HorarioBase {
-}
+    public abstract class HorarioBase {
+        private String diaSemana;
+        private String horaInicio;
+        private String horaFim;
+
+        public HorarioBase(String diaSemana, String horaInicio, String horaFim) {
+            this.diaSemana = diaSemana;
+            this.horaInicio = horaInicio;
+            this.horaFim = horaFim;
+        }
+
+        public String getDiaSemana() {
+            return diaSemana;
+        }
+
+        public void setDiaSemana(String diaSemana) {
+            this.diaSemana = diaSemana;
+        }
+
+        public String getHoraInicio() {
+            return horaInicio;
+        }
+
+        public void setHoraInicio(String horaInicio) {
+            this.horaInicio = horaInicio;
+        }
+
+        public String getHoraFim() {
+            return horaFim;
+        }
+
+        public void setHoraFim(String horaFim) {
+            this.horaFim = horaFim;
+        }
+
+        @Override
+        public String toString() {
+            return diaSemana + " das " + horaInicio + " às " + horaFim;
+        }
+    }
+
