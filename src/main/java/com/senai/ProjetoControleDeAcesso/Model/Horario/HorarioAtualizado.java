f21e6838e0c29d86567a4f470ab565d0edca0732
Gabrieli da Silva Marcelino <gabih.12161@gmail.com>
2025-05-30 00:53:15 -0300

    Criação do horário atualizado - 29/05/2025

--- src/main/java/com/senai/ProjetoControleDeAcesso/Model/Horario/HorarioAtualizado.java
+++ src/main/java/com/senai/ProjetoControleDeAcesso/Model/Horario/HorarioAtualizado.java
@@ -1,4 +1,20 @@
 package com.senai.ProjetoControleDeAcesso.Model.Horario;
 
-public class HorarioAtualizado {
+public class Horario {
+    private String diaSemana;
+    private String horario;
+    private String turma;
+    private String disciplina;
+
+    public Horario(String diaSemana, String horario, String turma, String disciplina) {
+        this.diaSemana = diaSemana;
+        this.horario = horario;
+        this.turma = turma;
+        this.disciplina = disciplina;
+    }
+
+    public void ExibirGradeDeHorario() {
+        System.out.printf("Dia: %s | Hora: %s | Turma: %s | Disciplina: %s | Professor: %s\n",
+                diaSemana, horario, turma, disciplina.getNome(), disciplina.getProfessor().getNome());
+    }
 }
