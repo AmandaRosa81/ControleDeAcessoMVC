8400beca6ed2536d91b56d330941531eea907e33
Gabrieli da Silva Marcelino <gabih.12161@gmail.com>
2025-05-31 17:25:34 -0300

    Correção da entidade horário - 31/05/2025

--- src/main/java/com/senai/ProjetoControleDeAcesso/Model/Horario/HorarioAtualizado.java
+++ src/main/java/com/senai/ProjetoControleDeAcesso/Model/Horario/HorarioAtualizado.java
@@ -1,19 +1,53 @@
 package com.senai.ProjetoControleDeAcesso.Model.Horario;
 
-public class Horario {
+public class HorarioAtualizado {
     private String diaSemana;
     private String horario;
     private String turma;
     private String disciplina;
 
-    public Horario(String diaSemana, String horario, String turma, String disciplina) {
+    public String getDiaSemana() {
+        return diaSemana;
+    }
+
+    public void setDiaSemana(String diaSemana) {
+        this.diaSemana = diaSemana;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public HorarioAtualizado(String diaSemana, String horario, String turma, String disciplina) {
         this.diaSemana = diaSemana;
         this.horario = horario;
         this.turma = turma;
         this.disciplina = disciplina;
+
+
     }
 
-    public void ExibirGradeDeHorario() {
+    public void exibir() {
         System.out.printf("Dia: %s | Hora: %s | Turma: %s | Disciplina: %s | Professor: %s\n",
                 diaSemana, horario, turma, disciplina.getNome(), disciplina.getProfessor().getNome());
     }
